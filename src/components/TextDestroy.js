d4e607abbef24ec1011d513858388d1fde89309a
Aniket Kumar Verma <vermaaniket213@gmail.com>
2020-12-24 14:24:32 +0530

    some change

--- src/components/TextDestroy.js
+++ src/components/TextDestroy.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react'
 class TextDestroy extends Component {
     state = {  }
     render() { 
+        let textArr=this.props.text && this.props.text.split(" ")
         return ( 
             <div>
-                {this.props.text && <h1>{this.props.text}</h1>}
+                {this.props.text && <div>
+                        {textArr && textArr.map((ele,index)=>{
+                            return(
+                                <span key={index}>{ele}</span>
+                            )
+                        })}
+                </div>}
             </div>
          );
     }
